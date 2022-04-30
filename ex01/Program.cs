84bfd2da540f7feda878f09267c3e077438cf3f8
Andrey <kaverin97andry@mail.ru>
2022-04-30 19:31:19 +0300

    Add ex01

---
+++ ex01/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Написать прог, кот на вход приним два числа 
+и показ кот из них большее, а какое меньшее*/
+Console.WriteLine("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if (numberA < numberB)
+{
+    Console.WriteLine(max = numberB);
+}
+
+else
+{
+    Console.WriteLine(max = numberA);
+}
